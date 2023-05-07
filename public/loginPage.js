64f39b7c4fac3817275253a3c187b3d381460dbe
Ekaterina Bakaeva <bakaeva.es@gmail.com>
2023-05-07 13:42:50 +0300

    LoginPage finished

---
+++ public/loginPage.js
@@ -0,0 +1,21 @@
+"use strict"
+let user = new UserForm();
+user.loginFormCallback = data => {
+	ApiConnector.login(data, response => {
+		if (response.success) {
+			location.reload();
+		} else {
+			user.setLoginErrorMessage(response.error);
+		}
+	})
+}
+user.registerFormCallback = data => {
+	ApiConnector.register(data, response => {
+		if (response.success) {
+			location.reload();
+		} else {
+			user.setRegisterErrorMessage(response.error);
+		}
+	})
+}
+//oleg@demo.ru
